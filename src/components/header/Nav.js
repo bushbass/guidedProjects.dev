7af15b366ad2dcd94ca8be1248acadc36c7d3f95
Alex Nielsen <alex.nielsen@gmail.com>
2021-02-23 19:02:30 -0500

    Initial commit

---
+++ src/components/header/Nav.js
@@ -0,0 +1,17 @@
+export default function Nav() {
+  return (
+    <nav>
+      <ul>
+        <a href="">
+          <li>Home</li>
+        </a>
+        <a href="">
+          <li>Deals</li>
+        </a>
+        <a href="">
+          <li>Cart</li>
+        </a>
+      </ul>
+    </nav>
+  );
+}
