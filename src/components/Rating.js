9e886cddd39a118ac2fa9ef48a9acaffa1251cd0
bushbass <alex.nielsen@gmail.com>
2021-02-27 10:52:00 -0500

    added star rating component

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,14 +1,110 @@
 import StarFull from '../images/star_full.svg';
-import StarHalf from '../images/star-empty.svg';
-import StarEmpty from '../images/star-half.svg';
+import StarEmpty from '../images/star-empty.svg';
+import StarHalf from '../images/star-half.svg';
 
 export default function Rating({ stars }) {
   return (
     <div>
-      <p>{stars} Stars</p>
-      <img src={StarFull} style={{ height: '20px' }} />
-      <img src={StarHalf} style={{ height: '20px' }} />
-      <img src={StarEmpty} style={{ height: '20px' }} />
+      {stars === 0 ? (
+        <>
+          <img src={StarEmpty} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+        </>
+      ) : null}
+      {stars === 0.5 ? (
+        <>
+          <img src={StarHalf} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+        </>
+      ) : null}
+      {stars === 1 ? (
+        <>
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+        </>
+      ) : null}
+      {stars === 1.5 ? (
+        <>
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarHalf} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+        </>
+      ) : null}
+      {stars === 2 ? (
+        <>
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+        </>
+      ) : null}
+      {stars === 2.5 ? (
+        <>
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarHalf} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+        </>
+      ) : null}
+      {stars === 3 ? (
+        <>
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+        </>
+      ) : null}
+      {stars === 3.5 ? (
+        <>
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarFull} style={{ height: '30px' }} />
+
+          <img src={StarHalf} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+        </>
+      ) : null}
+      {stars === 4 ? (
+        <>
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarEmpty} style={{ height: '30px' }} />
+        </>
+      ) : null}
+      {stars === 4.5 ? (
+        <>
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarHalf} style={{ height: '30px' }} />
+        </>
+      ) : null}
+      {stars === 5 ? (
+        <>
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarFull} style={{ height: '30px' }} />
+          <img src={StarFull} style={{ height: '30px' }} />
+        </>
+      ) : null}
     </div>
   );
 }
