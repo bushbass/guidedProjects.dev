78c7be3885362d9ca65f6374c06e567881701fb9
Alex Nielsen <alex@bigal.org>
2021-03-03 15:21:17 -0500

    added basic cart context

--- src/App.js
+++ src/App.js
@@ -1,41 +1,44 @@
-import { useEffect, useState } from "react";
-import "./styles.css";
-import Header from "./components/header/Header";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Cart from "./pages/Cart";
-import Home from "./pages/Home";
-import Deals from "./pages/Deals";
-import Item from "./pages/Item";
+import { useEffect, useState, useContext } from 'react';
+import './styles.css';
+import CartContext from './context/CartContext';
+import Header from './components/header/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import Deals from './pages/Deals';
+import Item from './pages/Item';
 
 export default function App() {
   useEffect(() => {
-    fetch("https://gp-super-store-api.herokuapp.com/item/list")
+    fetch('https://gp-super-store-api.herokuapp.com/item/list')
       .then((res) => res.json())
       .then((data) => setitemList(data.items));
   }, []);
   const [itemList, setitemList] = useState([]);
-
+  const [cart, setCart] = useState([{ product: 'fake initial product' }]);
   return (
     <div className="App">
-      <Router>
-        <div>
-          <Header />
-          <Switch>
-            <Route path="/deals">
-              <Deals itemList={itemList} />
-            </Route>
-            <Route path="/cart">
-              <Cart />
-            </Route>
-            <Route path="/item/:id">
-              <Item itemList={itemList} />
-            </Route>
-            <Route path="/">
-              <Home itemList={itemList} />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      <CartContext.Provider value={{ cart, setCart }}>
+        <Router>
+          <div>
+            <Header />
+            <Switch>
+              <Route path="/deals">
+                <Deals itemList={itemList} />
+              </Route>
+              <Route path="/cart">
+                <Cart />
+              </Route>
+              <Route path="/item/:id">
+                <Item itemList={itemList} />
+              </Route>
+              <Route path="/">
+                <Home itemList={itemList} />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </CartContext.Provider>
     </div>
   );
 }
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,8 +1,14 @@
+import { useContext } from 'react';
+import CartContext from '../context/CartContext';
+
 export default function Cart() {
+  const { cart } = useContext(CartContext);
+
   return (
     <div className="cart">
       <div>
         <h2>cart component</h2>
+        {console.log(cart)}
       </div>
     </div>
   );
