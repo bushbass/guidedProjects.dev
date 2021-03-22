6a555425799f8dc521e57788128cb1b55d9fd752
Alex Nielsen <alex@bigal.org>
2021-03-22 13:03:03 -0400

    kind of fixed dropdown qty but saving becauase I think I want to get rid of it afterall

--- src/components/QuantityDropdown.js
+++ src/components/QuantityDropdown.js
@@ -2,33 +2,43 @@ import React, { useEffect, useState } from 'react';
 
 function QuantityDropdown({ inStockValue }) {
   const [qtyDropdown, setQtyDropdown] = useState(1);
-  const [stockArr, setStockArr] = useState((inStockValue) => {
-    let stock = [...Array(inStockValue).keys()];
-    stock.shift();
-    return stock;
-  });
+  const [selectedDropdownValue, setSelectedDropdownValue] = useState(1);
+  const [quantityArr, setQuantityArr] = useState([]);
   const [stockValue, setStockValue] = useState(0);
 
+  useEffect(() => {
+    let tempArr = [];
+    for (let i = 1; i <= inStockValue; i++) {
+      tempArr.push(i);
+      console.log(tempArr);
+    }
+    setQuantityArr(tempArr);
+    // let stock = [...Array(inStockValue).keys()];
+    // stock.shift();
+    // console.log(stock);
+  }, []);
+
   function handleDropdownChange(e) {
-    setQtyDropdown(e.target.value);
+    setSelectedDropdownValue(e.target.value);
+    setQtyDropdown(1);
   }
   return (
     <div>
-      {console.log(stockArr)}
+      {console.log({ inStockValue, quantityArr })}
       <label htmlFor="qtyDropdown">Quantity to add:</label>
       <select
         value={qtyDropdown}
         onChange={handleDropdownChange}
         name="qtyDropdown"
       >
-        {stockArr.map((item, id) => (
-          <option key={id} value={item + 1}>
-            {item + 1}
+        {quantityArr.map((item, id) => (
+          <option key={id} value={item}>
+            {item}
           </option>
         ))}
       </select>
 
-      <div>{qtyDropdown}</div>
+      <div>{selectedDropdownValue}</div>
     </div>
   );
 }
--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,26 +1,25 @@
-import React, { useState } from 'react';
-function TestPage(inStockValue) {
-  const total = 3;
-  const [newArr, setNewArr] = useState([...Array(total).keys()]);
-  const [qtyDropdown, setQtyDropdown] = useState(1);
-  function handleDropdownChange(e) {
-    setQtyDropdown(e.target.value);
-  }
+import React, { useState, useEffect } from 'react';
+
+function TestPage() {
+  const total = Array(5);
+  const [dropDownItems, setDropDownItems] = useState('0');
+  useEffect(() => {
+    setDropDownItems(dropDownItems + `option value=${i}>${i}</option>`);
+    console.log(i + 1);
+  }, []);
+
   return (
     <div>
-      <label htmlFor="qtyDropdown">Quantity:</label>
-      <select
-        value={qtyDropdown}
-        onChange={handleDropdownChange}
-        name="qtyDropdown"
-        id="qtyDropdown"
-      >
-        {newArr.map((item) => (
-          <option value={item + 1}>{item + 1}</option>
-        ))}
-      </select>
-
-      <div>{qtyDropdown}</div>
+      <label htmlFor="cars">Choose a car:</label>
+      {console.log(dropDownItems)}
+      {/* {}
+      <select name="cars" id="cars">
+          {total.map((item,id)=>option value={id}>{id}</option>`)}
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="mercedes">Mercedes</option>
+        <option value="audi">Audi</option>
+      </select> */}
     </div>
   );
 }
