99da765a0efe74dd7885ee7ae8bec214777abf16
Alex Nielsen <alex.nielsen@gmail.com>
2021-03-31 20:41:15 -0400

    made pagination buttons conditionally render so they don't show Nan

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 const Button = styled.button`
   margin: 3px;
 `;
@@ -9,7 +9,7 @@ function Pagination({
   setPageCount,
   total,
   pageCount,
-  next,
+  next
 }) {
   function handlePageSizeChange(e) {
     if (e.target.value < 1) return setPageSize(1);
@@ -18,31 +18,33 @@ function Pagination({
   }
   return (
     <>
-      <div>
-        {console.log({ total })}
-        <Button
-          disabled={next >= 0 && next < 4 && next !== null}
-          onClick={() => setPageCount(0)}
-        >
-          First
-        </Button>
-        <Button
-          disabled={next >= 0 && next < 4 && next !== null}
-          onClick={() => setPageCount(pageCount - 2)}
-        >
-          &lt;
-        </Button>
-        <span>
-          {pageCount / 2 + 1} of {Math.ceil(total / 2)}
-        </span>
+      {!total ? null : (
+        <div>
+          {console.log({ total })}
+          <Button
+            disabled={next >= 0 && next < 4 && next !== null}
+            onClick={() => setPageCount(0)}
+          >
+            First
+          </Button>
+          <Button
+            disabled={next >= 0 && next < 4 && next !== null}
+            onClick={() => setPageCount(pageCount - 2)}
+          >
+            &lt;
+          </Button>
+          <span>
+            {pageCount / 2 + 1} of {Math.ceil(total / 2)}
+          </span>
 
-        <Button disabled={!next} onClick={() => setPageCount(pageCount + 2)}>
-          &gt;
-        </Button>
-        <Button disabled={!next} onClick={() => setPageCount(total - 1)}>
-          Last
-        </Button>
-      </div>
+          <Button disabled={!next} onClick={() => setPageCount(pageCount + 2)}>
+            &gt;
+          </Button>
+          <Button disabled={!next} onClick={() => setPageCount(total - 1)}>
+            Last
+          </Button>
+        </div>
+      )}
       {/* <div>
         <label htmlFor="pageSize">Items per page</label>
         <input
